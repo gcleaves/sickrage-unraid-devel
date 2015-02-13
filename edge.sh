38d8a1eaec4abb83e828c78dda70872ffd772e15
Geoffrey Cleaves <gcleaves@gcleaves.com>
2015-02-13 23:33:31 +0100

    cloning develop branch

--- edge.sh
+++ edge.sh
@@ -6,6 +6,7 @@ if [ -z "$EDGE" ]; then
 else
   apt-get install -qy git
   rm -rf /opt/sickrage
-  git clone https://github.com/SiCKRAGETV/SickRage.git /opt/sickrage
+  #git clone https://github.com/SiCKRAGETV/SickRage.git /opt/sickrage
+  git clone -b develop --single-branch https://github.com/SiCKRAGETV/SickRage.git /opt/sickrage
   chown -R nobody:users /opt/sickrage
 fi
